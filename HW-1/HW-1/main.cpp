8b70abb6acbae566bb8d460d6843434988c1e9c4
Carlos D. Puchol <cpt@creeper.amahi.net>
2014-09-22 18:17:04 -0700

    HW - 1

---
+++ HW-1/HW-1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HW-1
+//
+//  Created by Carlos D. Puchol on 9/22/14.
+//  Copyright (c) 2014 Carlos D. Puchol. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

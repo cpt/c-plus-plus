9ab780423ac8496cbcdcff6ee09799093cee98a7
Carlos D. Puchol <thepbrothers@yahoo.com>
2014-09-29 18:52:31 -0700

    Created hello world program.

---
+++ HW-1/Hello World/Hello World/main.cpp
@@ -0,0 +1,15 @@
+// Prints message "Hello, World!" to the console.
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World! \n";
+    return 0;
+}
+
+/*
+Hello, World!
+Program ended with exit code: 0
+*/

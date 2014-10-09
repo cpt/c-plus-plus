4a16850844587192947280ef894e4a8ccefd8c45
Carlos Puchol <cpg@rocketmail.com>
2014-10-09 15:20:31 -0700

    minor changes for readability/style

--- HW-1/BMI Index/BMI Index/main.cpp
+++ HW-1/BMI Index/BMI Index/main.cpp
@@ -3,36 +3,33 @@
  Then the program displays the user's BMI according to the data provided.
  */
 
-
 #include <iostream>
 
 using namespace std;
 
-
-
 int main (void) {
     int userHeight;
+    
+    // Get the height of the user in cm and save it as an int.
     cout << "Please type your height in centimeters: " ;
     cin  >> userHeight;
-    // Gets the height of user in cm and saves it as an int.
-    
     
     int userWeight;
+    
+    // Get the weight of the user in kg and save it as an int.
     cout << "Please type your weight in kilograms: " ;
     cin >> userWeight;
-    // Gets the weight of the user in kg and saves it as an int.
-
+    
+    // A "double" variable to build up the BMI.
     double bmi;
-    //Creats a "double" variable "bmi".
     
-    bmi = ((double)userHeight)/100.0;
+    // Piecemeal computation to run users data through equation.
+    bmi = ((double) userHeight)/100.0;
     bmi = bmi * bmi;
-    bmi = ((double) userWeight) / (bmi);
-    //Piecemeal computation to run users data through equation.
+    bmi = ((double) userWeight)/bmi;
     
-
+    // Prints the user's BMI.
     cout << "Your BMI is: " << bmi << "Kg/M^2" << endl;
-    //Prints the users BMI.
     
     return 0;
 }
